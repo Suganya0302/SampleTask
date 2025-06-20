81429ac675618f03acd25d2e7f5c99e66b30eb2d
pradeep0627 <pradeepcj2706@gmail.com>
2025-06-20 10:56:39 +0530

    pradeep's code

--- src/test/java/org/day/Day1.java
+++ src/test/java/org/day/Day1.java
@@ -3,6 +3,8 @@ package org.day;
 public class Day1 {
 	public static void main(String[] args) {
 		System.out.println("test1");
+		System.out.println("test2");
+		System.out.println("test3");
 	}
 
 }
